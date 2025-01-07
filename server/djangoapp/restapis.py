1dee2d8a0a6bb52f06c6c08ab03b35c965470326
Jentzen Pablo <jentzen2004@gmail.com>
2025-01-07 04:42:07 -0500

    Hocus Pocus

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -30,8 +30,35 @@ def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
 # def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
 # Add code for posting review
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
